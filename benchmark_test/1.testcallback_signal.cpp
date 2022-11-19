3108e8f00fa24e1040e65ed7a85b0f69aa087bf5
lixiaoxmm <lixiaoxmm@163.com>
2022-11-20 01:25:05 +0800

    add core/signal.hpp and benchmark_test/1.testcallback_signal.cpp

---
+++ benchmark_test/1.testcallback_signal.cpp
@@ -0,0 +1,147 @@
+#include <iostream>
+#include <memory>
+#include <utility>
+
+#include "catch.hpp"
+#include "core/signal.hpp"
+class MyInt {
+ public:
+  template <typename T>
+  MyInt(const T value) : value(value) {}
+  std::uint32_t getValue() const { return value; }
+  bool operator==(const MyInt& b) const noexcept {
+    return getValue() == b.getValue();
+  }
+  bool operator<(const MyInt& b) const noexcept {
+    return getValue() < b.getValue();
+  }
+
+ private:
+  std::uint32_t value;
+};
+
+namespace std {
+template <>
+struct hash<MyInt> {
+  std::size_t operator()(const MyInt& value) const noexcept {
+    return std::hash<std::uint32_t>()(value.getValue());
+  }
+};
+}  // namespace std
+
+TEST_CASE("test_slot") {
+  std::uint32_t sum = 0;
+  std::uint32_t lhs = 1, rhs = 2;
+  XEngine::Slot<bool(std::uint32_t, std::uint32_t)> slot_a(
+      [&sum](std::uint32_t lhs, std::uint32_t rhs) -> bool {
+        sum = lhs + rhs;
+        return false;
+      });
+  slot_a.connected();
+  REQUIRE(!slot_a(lhs, rhs));
+  REQUIRE(sum == lhs + rhs);
+
+  XEngine::Slot<void(std::uint32_t, std::uint32_t)> slot_b(
+      [&sum](std::uint32_t lhs, std::uint32_t rhs) -> void {});
+  slot_b.connected();
+  REQUIRE(std::is_same<void, decltype(slot_b(lhs, rhs))>::value);
+
+  XEngine::Slot<std::string(std::uint32_t, std::uint32_t)> slot_c(
+      [&sum](std::uint32_t lhs, std::uint32_t rhs) -> std::string {
+        return std::to_string(lhs) + std::to_string(rhs);
+      });
+  slot_c.connected();
+  REQUIRE(slot_c(lhs, rhs) == std::to_string(lhs) + std::to_string(rhs));
+}
+
+TEST_CASE("test_signal") {
+  std::uint32_t step{};
+  XEngine::Signal<bool(std::uint32_t, std::uint32_t)> sig;
+  sig.Connect([&step](std::uint32_t lhs, std::uint32_t rhs) -> bool {
+    ++step;
+    return true;
+  });
+  sig.Connect([&step](std::uint32_t lhs, std::uint32_t rhs) -> bool {
+    ++step;
+    return true;
+  });
+  sig.Connect([&step](std::uint32_t lhs, std::uint32_t rhs) -> bool {
+    ++step;
+    return true;
+  });
+  REQUIRE(step == 0);
+  REQUIRE(sig(1, 2));
+  REQUIRE(step == 3);
+
+  step = 0;
+  sig.Connect(
+      [&step](std::uint32_t lhs, std::uint32_t rhs) -> bool { return false; });
+  sig.Connect([&step](std::uint32_t lhs, std::uint32_t rhs) -> bool {
+    ++step;
+    return true;
+  });
+  REQUIRE(!sig(1, 2));
+  REQUIRE(step == 3);
+}
+
+TEST_CASE("test_dispatcher_singnal") {
+  std::uint32_t sum{};
+  XEngine::Dispatcher<std::uint32_t, bool(std::uint32_t, std::uint32_t)>
+      dispatcher_a;
+  dispatcher_a.AddListener(1, [&sum](std::uint32_t a, std::uint32_t b) -> bool {
+    ++sum;
+    return false;
+  });
+  dispatcher_a.AddListener(2, [&sum](std::uint32_t a, std::uint32_t b) -> bool {
+    ++sum;
+    return true;
+  });
+  REQUIRE(!dispatcher_a.Dispatch(1, 1, 1));
+  REQUIRE(sum == 1);
+  REQUIRE(dispatcher_a.Dispatch(2, 1, 1));
+  REQUIRE(sum == 2);
+
+  std::string str{};
+  XEngine::Dispatcher<std::string, bool(std::uint32_t, std::uint32_t)>
+      dispatcher_b;
+  dispatcher_b.AddListener("1",
+                           [&str](std::uint32_t a, std::uint32_t b) -> bool {
+                             str = std::to_string(a) + std::to_string(b);
+                             return false;
+                           });
+  dispatcher_b.AddListener("2",
+                           [&str](std::uint32_t a, std::uint32_t b) -> bool {
+                             str = std::to_string(a) + std::to_string(b);
+                             return true;
+                           });
+
+  REQUIRE(!dispatcher_b.Dispatch("1", 1, 1));
+  REQUIRE(str == std::to_string(1) + std::to_string(1));
+  REQUIRE(dispatcher_b.Dispatch("2", 2, 2));
+  REQUIRE(str == std::to_string(2) + std::to_string(2));
+
+  XEngine::Dispatcher<std::string, void(std::uint32_t, std::uint32_t)>
+      dispatcher_c;
+  dispatcher_c.AddListener("1",
+                           [&](std::uint32_t a, std::uint32_t b) -> void {});
+  dispatcher_c.AddListener("2",
+                           [&](std::uint32_t a, std::uint32_t b) -> void {});
+  REQUIRE(
+      std::is_same<void, decltype(dispatcher_c.Dispatch("1", 1, 1))>::value);
+  REQUIRE(
+      std::is_same<void, decltype(dispatcher_c.Dispatch("2", 2, 2))>::value);
+
+  sum = 0;
+  XEngine::Dispatcher<MyInt, void(std::uint32_t, std::uint32_t)> dispatcher_d;
+  dispatcher_d.AddListener(
+      MyInt(1), [&sum](std::uint32_t a, std::uint32_t b) -> void { ++sum; });
+  dispatcher_d.AddListener(
+      MyInt(1), [&sum](std::uint32_t a, std::uint32_t b) -> void { ++sum; });
+  dispatcher_d.AddListener(
+      MyInt(1), [&sum](std::uint32_t a, std::uint32_t b) -> void { ++sum; });
+  dispatcher_d.Dispatch(MyInt(1), 1, 1);
+  REQUIRE(sum == 3);
+
+  dispatcher_d.Dispatch(MyInt(1), 1, 1);
+  REQUIRE(sum == 6);
+}
---
+++ core/signal.hpp
@@ -0,0 +1,307 @@
+/***************************
+@Author: Xhosa-LEE
+@Contact: lixiaoxmm@163.com
+@Time: 2022/11/20
+@Desc: 信号与槽接口
+***************************/
+
+#pragma once
+
+#include <functional>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <shared_mutex>
+#include <utility>
+
+namespace XEngine {
+
+template <typename... Args>
+class Connection;
+template <typename T>
+class Signal;
+template <typename T>
+class Slot;
+template <typename Key, typename Value>
+class Dispatcher;
+
+template <typename... Args>
+class SlotBase {
+ public:
+  using Callback = std::function<void(Args...)>;
+  SlotBase(const SlotBase& another)
+      : cb_(another.cb_), connected_(another.connected_) {}
+  explicit SlotBase(const Callback& cb, bool connected = true)
+      : cb_(cb), connected_(connected) {}
+  virtual ~SlotBase() {}
+
+  void operator()(Args... args) {
+    if (connected_ && cb_) {
+      cb_(std::forward<Args>(args)...);
+    }
+  }
+
+  void Disconnect() { connected_ = false; }
+  bool connected() const { return connected_; }
+
+ private:
+  Callback cb_;
+  bool connected_ = true;
+};
+
+template <typename RT, typename... Args>
+class Slot<RT(Args...)> : public SlotBase<Args...> {
+  using Callback = std::function<RT(Args...)>;
+
+ public:
+  explicit Slot(const Callback& call_back, bool connected = true)
+      : SlotBase<Args...>(nullptr, connected), call_back_(call_back) {}
+  virtual ~Slot() {}
+
+  RT operator()(Args... args) {
+    return CallBack<RT>(std::forward<Args>(args)...);
+  }
+
+ private:
+  template <typename Rt>
+  auto CallBack(Args... args) const ->
+      typename std::enable_if<!std::is_void<RT>::value, Rt>::type {
+    if (SlotBase<Args...>::connected() && call_back_) {
+      return call_back_(std::forward<Args>(args)...);
+    }
+    return Rt{};
+  }
+
+  template <typename Rt>
+  auto CallBack(Args... args) const ->
+      typename std::enable_if<std::is_void<Rt>::value, Rt>::type {
+    if (SlotBase<Args...>::connected() && call_back_) {
+      return call_back_(std::forward<Args>(args)...);
+    }
+  }
+
+  Callback call_back_;
+};
+
+template <typename RT, typename... Args>
+class Signal<RT(Args...)> {
+  using Callback = std::function<RT(Args...)>;
+  using SlotPtr = std::shared_ptr<Slot<RT(Args...)>>;
+  using SlotList = std::list<SlotPtr>;
+  using ConnectionType = Connection<RT(Args...)>;
+
+ public:
+  explicit Signal(const Callback& call_back) { Connect(call_back); }
+  Signal() {}
+  virtual ~Signal() { DisconnectAllSlots(); }
+
+  RT operator()(Args... args) {
+    return CallBack<RT>(std::forward<Args>(args)...);
+  }
+
+  ConnectionType Connect(const Callback& call_back) {
+    auto slot = std::make_shared<Slot<RT(Args...)>>(call_back);
+    {
+      std::lock_guard<std::mutex> lock(mutex_);
+      slots_.emplace_back(slot);
+    }
+
+    return ConnectionType(slot, this);
+  }
+
+  bool Disconnect(const ConnectionType& conn) {
+    bool find = false;
+    {
+      std::lock_guard<std::mutex> lock(mutex_);
+      for (auto& slot : slots_) {
+        if (conn.HasSlot(slot)) {
+          find = true;
+          slot->Disconnect();
+        }
+      }
+    }
+
+    if (find) {
+      ClearDisconnectedSlots();
+    }
+    return find;
+  }
+
+  void DisconnectAllSlots() {
+    std::lock_guard<std::mutex> lock(mutex_);
+    for (auto& slot : slots_) {
+      slot->Disconnect();
+    }
+    slots_.clear();
+  }
+
+ private:
+  template <typename Rt>
+  auto CallBack(Args... args) ->
+      typename std::enable_if<!std::is_void<RT>::value, Rt>::type {
+    SlotList local;
+    {
+      std::lock_guard<std::mutex> lock(mutex_);
+      for (auto& slot : slots_) {
+        local.emplace_back(slot);
+      }
+    }
+    Rt ret{};
+    if (!local.empty()) {
+      for (auto& slot : local) {
+        ret = (*slot)(std::forward<Args>(args)...);
+        if (!ret) {
+          return ret;
+        }
+      }
+    }
+    ClearDisconnectedSlots();
+    return ret;
+  }
+
+  template <typename Rt>
+  auto CallBack(Args... args) ->
+      typename std::enable_if<std::is_void<Rt>::value, Rt>::type {
+    SlotList local;
+    {
+      std::lock_guard<std::mutex> lock(mutex_);
+      for (auto& slot : slots_) {
+        local.emplace_back(slot);
+      }
+    }
+
+    if (!local.empty()) {
+      for (auto& slot : local) {
+        (*slot)(std::forward<Args>(args)...);
+      }
+    }
+
+    ClearDisconnectedSlots();
+  }
+  Signal(const Signal&) = delete;
+  Signal& operator=(const Signal&) = delete;
+
+  void ClearDisconnectedSlots() {
+    std::lock_guard<std::mutex> lock(mutex_);
+    slots_.erase(
+        std::remove_if(slots_.begin(), slots_.end(),
+                       [](const SlotPtr& slot) { return !slot->connected(); }),
+        slots_.end());
+  }
+  SlotList slots_;
+  std::mutex mutex_;
+};
+
+template <typename RT, typename... Args>
+class Connection<RT(Args...)> {
+  using SlotPtr = std::shared_ptr<Slot<RT(Args...)>>;
+  using SignalPtr = Signal<RT(Args...)>*;
+
+ public:
+  Connection() : slot_(nullptr), signal_(nullptr) {}
+  Connection(const SlotPtr& slot, const SignalPtr& signal)
+      : slot_(slot), signal_(signal) {}
+  virtual ~Connection() {
+    slot_ = nullptr;
+    signal_ = nullptr;
+  }
+
+  Connection& operator=(const Connection& another) {
+    if (this != &another) {
+      this->slot_ = another.slot_;
+      this->signal_ = another.signal_;
+    }
+    return *this;
+  }
+
+  bool HasSlot(const SlotPtr& slot) const {
+    if (slot != nullptr && slot_ != nullptr) {
+      return slot_.get() == slot.get();
+    }
+    return false;
+  }
+
+  bool IsConnected() const {
+    if (slot_) {
+      return slot_->connected();
+    }
+    return false;
+  }
+
+  bool Disconnect() {
+    if (signal_ && slot_) {
+      return signal_->Disconnect(*this);
+    }
+    return false;
+  }
+
+ private:
+  SlotPtr slot_;
+  SignalPtr signal_;
+};
+
+template <typename EventType, typename RT, typename... Args>
+class Dispatcher<EventType, RT(Args...)> {
+  using Callback = std::function<RT(Args...)>;
+  using Mutex = std::shared_mutex;
+  using SignalPtr = std::shared_ptr<Signal<RT(Args...)>>;
+  using Map = std::map<EventType, SignalPtr>;
+
+ public:
+  bool AddListener(const EventType& event, const Callback& call_back) {
+    std::lock_guard<Mutex> lockGuard(rw_lock_);
+    if (!HasChannel(event)) {
+      SignalPtr signal_ptr = std::make_shared<Signal<RT(Args...)>>(call_back);
+      msg_listeners_.insert(std::pair<EventType, SignalPtr>(event, signal_ptr));
+    } else {
+      msg_listeners_[event]->Connect(call_back);
+    }
+
+    return true;
+  }
+
+  RT Dispatch(const EventType& event, Args... args) {
+    return CallBack<RT>(event, std::forward<Args>(args)...);
+  }
+
+  bool RemoveListener(const EventType& event) {
+    std::lock_guard<Mutex> lockGuard(rw_lock_);
+    if (HasChannel(event)) {
+      msg_listeners_.erase(event);
+    }
+    return true;
+  }
+
+  bool HasChannel(const EventType& event) {
+    auto listener = msg_listeners_.find(event);
+    if (listener != msg_listeners_.end()) {
+      return true;
+    }
+    return false;
+  }
+
+ private:
+  template <typename Rt>
+  auto CallBack(const EventType& event, Args... args) ->
+      typename std::enable_if<std::is_void<RT>::value, Rt>::type {
+    std::shared_lock<std::shared_mutex> lock(rw_lock_);
+    if (HasChannel(event)) {
+      (*msg_listeners_[event])(std::forward<Args>(args)...);
+    }
+  }
+
+  template <typename Rt>
+  auto CallBack(const EventType& event, Args... args) ->
+      typename std::enable_if<!std::is_void<Rt>::value, Rt>::type {
+    std::shared_lock<std::shared_mutex> lock(rw_lock_);
+    if (HasChannel(event)) {
+      return (*msg_listeners_[event])(std::forward<Args>(args)...);
+    }
+    return RT{};
+  }
+
+  Map msg_listeners_;
+  mutable Mutex rw_lock_;
+};
+}  // namespace XEngine
