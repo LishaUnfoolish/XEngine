1da94f5a5bf5e109d9503a40200e2a3c12d33f26
Xhosa-LEE <39613608+LishaUnfoolish@users.noreply.github.com>
2022-11-22 02:48:45 +0800

    Update type_info.hpp

--- core/type_info.hpp
+++ core/type_info.hpp
@@ -1,7 +1,7 @@
 /***************************
 @Author: Xhosa-LEE
 @Contact: lixiaoxmm@163.com
-@Time: 2022/11/20
+@Time: 2022/11/22
 @Desc: 静态类型标识
 ***************************/
 #pragma once
@@ -15,11 +15,16 @@ using type_index = std::type_index;
 using id_type = std::hash<std::type_index>::result_type;
 
 template <typename Type, typename = void>
-struct type_hash final {
+struct TypeHash final {
   [[nodiscard]] static constexpr id_type value() noexcept {
     return std::hash<std::type_index>{}(typeid(Type));
   }
   [[nodiscard]] constexpr operator id_type() const noexcept { return value(); }
 };
+
+template <auto Value>
+using integral_constant = std::integral_constant<decltype(Value), Value>;
+template <id_type Value>
+using tag = integral_constant<Value>;
 
 }  // namespace XEngine
