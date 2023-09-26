39a55ac23abc3679247695cac28130f50c5ba23e
lixiaoxmm <lixiaoxmm@163.com>
2023-09-26 20:57:39 +0800

    Fix

--- test_bazel/test_opencv/my_test.cpp
+++ test_bazel/test_opencv/my_test.cpp
@@ -1,9 +1,11 @@
 #include <opencv4/opencv2/opencv.hpp>
 
 int main(int argc, char** argv) {
-  // 从参数中获取图片路径
-  std::string filename = "/xengine/test_bazel/test_opencv/test.jpg";
-  if (argc == 2) { filename = argv[1]; }
+    // 从参数中获取图片路径
+  std::string filename="/xengine/test_bazel/test_opencv/test.jpg";
+  if (argc == 2) {
+    filename = argv[1];
+  }
   cv::Mat img = cv::imread(filename);
   if (img.empty()) {
     printf("Could not load image\n");
@@ -12,4 +14,4 @@ int main(int argc, char** argv) {
   cv::imshow("image", img);
   cv::waitKey(0);
   return 0;
-}
+} 
--- third_party/opencv4.BUILD
+++ third_party/opencv4.BUILD
@@ -40,6 +40,7 @@ cc_library(
         "-pthread",
     ],
     deps = [
+        # "@zlib",
     ],
 )
 
